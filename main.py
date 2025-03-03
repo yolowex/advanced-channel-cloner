c32d0a8263499dd1ce1b8b13cef7120498fd9514
yolowex <122750743+yolowex@users.noreply.github.com>
2025-03-03 14:08:18 +0330

    Filtered out some message types and add captions to media

--- main.py
+++ main.py
@@ -34,9 +34,6 @@ else:
         except typer.Abort:
             exit(1)
 
-SENT_MEDIA_GROUP = []
-media_group_locks = {}
-
 in_memory = False
 
 if os.path.exists("tb_session.session"):
@@ -53,37 +50,6 @@ logging.basicConfig(level=logging.INFO)
 app = Client("tb_session", api_id, api_hash, in_memory=in_memory)
 
 
-async def forward_media_group(message):
-    """
-    This function forwards a media group from one channel to another.
-    It assumes all channels have forwarding restricted and copies the content of the media group to the monitoring channel.
-
-    :param message: The message containing the media group
-    :type message: pyrogram.types.Message
-    """
-    # If there's no lock for this media group, create one
-    if message.media_group_id not in media_group_locks:
-        media_group_locks[message.media_group_id] = asyncio.Lock()
-
-    # Acquire the lock for this media group
-    async with media_group_locks[message.media_group_id]:
-        if message.media_group_id in SENT_MEDIA_GROUP:
-            return
-
-        SENT_MEDIA_GROUP.append(message.media_group_id)  # Because it was sent repeatedly for some reason
-        media_group = await app.get_media_group(message.chat.id, message.id)
-        v_media_group = []
-        for message in media_group:
-            logging.info("Downloading media")
-            photo = await app.download_media(message, in_memory=True)
-            photo_obj = InputMediaPhoto(photo)
-            v_media_group.append(photo_obj)
-        logging.info("Sending media group")
-        await app.send_media_group(target_channel, v_media_group)
-        print("Sent media group")
-        v_media_group.clear()
-
-
 async def handle_media(message):
     """
     This function handles different types of media in a message and forwards them to the source channel.
@@ -91,91 +57,49 @@ async def handle_media(message):
     :param message: The message containing the media
      :type message: pyrogram.types.Message
     """
-    if message.poll:
-        options = []
+    message_text = message.text if message.text is not None else ""
+    print(message_text)
 
-        # is it anonymous?
-        if message.poll.is_anonymous:
-            is_anonymous = True
-        else:
-            is_anonymous = False
+    # if message.poll:
 
-        for option in message.poll.options:
-            options.append(option.text)
-        await app.send_poll(target_channel, question=message.poll.question, options=options, is_anonymous=is_anonymous)
-        return
-
-    if message.sticker:
-        sticker = await app.download_media(message, in_memory=True)
-        await app.send_sticker(target_channel, sticker)
-        return
+    # if message.sticker:
 
     if message.video:
         video = await app.download_media(message, in_memory=True)
-        await app.send_video(target_channel, video)
+        await app.send_video(target_channel, video, caption=message_text, caption_entities=message.entities, )
         return
 
-    if message.audio:
-        audio = await app.download_media(message, in_memory=True)
-        await app.send_audio(target_channel, audio)
-        return
+    # if message.audio:
 
-    if message.voice:
-        voice = await app.download_media(message, in_memory=True)
-        await app.send_voice(target_channel, voice)
-        return
+    # if message.voice:
 
-    if message.animation:
-        animation = await app.download_media(message, in_memory=True)
-        await app.send_animation(target_channel, animation)
-        return
+    # if message.animation:
 
-    if message.document:
-        document = await app.download_media(message, in_memory=True)
-        await app.send_document(target_channel, document)
-        return
+    # if message.document:
 
-    if message.contact:
-        await app.send_contact(target_channel, message.contact.phone_number, message.contact.first_name)
-        return
+    # if message.contact:
 
-    if message.location:
-        await app.send_location(target_channel, message.location.latitude, message.location.longitude)
-        return
+    # if message.location:
 
-    if message.venue:
-        await app.send_venue(target_channel, message.venue.location.latitude, message.venue.location.longitude,
-                             message.venue.title, message.venue.address)
-        return
+    # if message.venue:
 
-    if message.game:
-        await app.send_game(target_channel, message.game.title)
-        return
+    # if message.game:
 
-    if message.video_note:
-        video_note = await app.download_media(message, in_memory=True)
-        await app.send_video_note(target_channel, video_note)
-        return
+    # if message.video_note:
 
-    if message.dice:
-        await app.send_dice(target_channel, message.dice.emoji)
-        return
+    # if message.dice:
 
     if message.photo:
         logging.info("Downloading media")
         photo = await app.download_media(message, in_memory=True)
         logging.info("Sending media")
-        await app.send_photo(target_channel, photo)
+        await app.send_photo(target_channel, photo, caption=message_text, caption_entities=message.entities,)
         return
 
 
 @app.on_message(filters.chat(source_channel_list))
 async def hello(client, message):
-    if message.media_group_id:
-        logging.info("Media group detected")
-        await forward_media_group(message)
-
-    elif message.media:
+    if message.media:
         logging.info("Media detected")
         await handle_media(message)
 
